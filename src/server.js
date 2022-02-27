7995672d0faf429d5712d0ce8c5f184c7017949a
Nathan Rhead Cox <nrc4@hotmail.com>
2022-02-27 14:14:09 -0800

    Add CORS headers in server.

--- src/server.js
+++ src/server.js
@@ -7,19 +7,13 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-// const allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-//   // intercept OPTIONS method
-//   if ('OPTIONS' == req.method) {
-//     res.send(200);
-//   }
-//   else {
-//     next();
-//   }
-// };
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 // http request logger-middleware for node.js
 const morgan = require('morgan');
